05d40a718d88ec858543fe298f962c5fe9c21a03
SriKumar982 <ksri33138@gmail.com>
2024-11-18 18:32:30 +0530

    employeelist  get

--- Frontend/src/components/dashboard/admin/EmployeeList.js
+++ Frontend/src/components/dashboard/admin/EmployeeList.js
@@ -1,41 +1,42 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
-
-import { DeleteForever, VisibilityOutlined, Logout } from '@mui/icons-material';
+import { DeleteForever } from '@mui/icons-material';
 import Sidebar from './Sidebar';
+import { Button } from 'antd';
 
 const EmployeeList = ({ setAuth }) => {
-  const [admins, setAdmins] = useState([]);
+  const [employees, setEmployees] = useState([]);
 
-  const getAdmins = async () => {
+  // Function to fetch all users and filter for employees
+  const getEmployees = async () => {
     try {
-      const response = await fetch('http://localhost:8000/allAdmins', {
+      const response = await fetch('http://localhost:5000/signup/getall', {
         method: 'GET',
         headers: { Authorization: localStorage.getItem('token') },
       });
 
-      const parseRes = await response.json();
+      const users = await response.json();
 
-      setAdmins(parseRes);
+      // Filter only users with userType 'employee'
+      const employeeUsers = users.filter((user) => user.userType === 'employee');
+      setEmployees(employeeUsers);
     } catch (error) {
       console.log(error);
     }
   };
-  console.log(admins);
 
-  // Delete ADMIN Function
+  // Delete Notification Function
   const deleteNotif = () => {
     toast.promise(
-      new Promise((resolve, reject) => {
+      new Promise((resolve) => {
         setTimeout(() => {
           resolve();
         }, 2000);
       }),
       {
-        pending: 'Deleting Admin...',
-        success: 'Deleted Succesfully!',
+        pending: 'Deleting Employee...',
+        success: 'Deleted Successfully!',
         error: 'Error!',
       },
       {
@@ -44,91 +45,77 @@ const EmployeeList = ({ setAuth }) => {
     );
   };
 
-  async function deleteAdmin(id) {
+  // Delete Employee Function
+  const deleteEmployee = async (id) => {
     try {
-      await fetch(`http://localhost:8000/admins/${id}`, {
+      await fetch(`http://localhost:5000/admins/${id}`, {
         method: 'DELETE',
         headers: { Authorization: localStorage.getItem('token') },
       });
       deleteNotif();
       setTimeout(() => {
-        setAdmins(admins.filter((admin) => admin.id !== id));
+        setEmployees(employees.filter((employee) => employee._id !== id));
       }, 2000);
     } catch (error) {
       console.log(error.message);
     }
-  }
+  };
 
   useEffect(() => {
-    getAdmins();
+    getEmployees();
   }, []);
 
   return (
-    <div className='w-full  border bg-white shadow-md rounded mt-5  border-t-4 border-t-red-500'>
-        <Sidebar/>
+    <div className="w-full border bg-white shadow-md rounded mt-5 border-t-4 border-t-red-500">
+      <Sidebar />
       <ToastContainer />
-      <div className='py-5 px-5'>
+      <div className="py-5 px-5">
         {/* TITLE */}
-        <div className='flex items-center justify-between border-b-2'>
-          <h3 className='text-lg font-medium  text-gray   px-1 '>
-            Manage Employees
-          </h3>
-          <button className='border hover:bg-red-700 bg-red-500 text-white font-bold py-2 px-4 mb-2 rounded focus:outline-none focus:shadow-outline mr-5'>
-            <Link to='/addAdmin' className='no-underline' style={{color:"white"}}>Add Employee</Link>
+        <div className="flex items-center justify-between border-b-2">
+          <h3 className="text-lg font-medium text-gray px-1">Manage Employees</h3>
+          <button className="border hover:bg-red-700 bg-red-500 text-white font-bold py-2 px-4 mb-2 rounded focus:outline-none focus:shadow-outline mr-5">
+            <Link to="/addAdmin" className="no-underline" style={{ color: 'white' }}>
+              Add Employee
+            </Link>
           </button>
         </div>
         {/* INFO */}
-        <div className='w-full  px-4   mt-5 overflow-auto hover:overflow-scroll border rounded shadow-md '>
-          <table className='table-fixed text-center mb-2'>
-            <thead className=' mt-5'>
-              <tr className=' mt-10'>
-                <th className='w-1/1 px-1 py-2 text-gray-600'>ID</th>
-                <th className='w-1/4 px-1 py-2 text-gray-600'>Full Name</th>
-                <th className='w-1/4 px-1 py-2 text-gray-600'>
-                  Contact Number
-                </th>
-                <th className='w-1/4 px-4 py-2 text-gray-600'>Address</th>
-                <th className='w-1/4 px-1 py-2 text-gray-600'>Email</th>
-                <th className='w-1/1 px-1 py-2 text-gray-600'>Action</th>
+        <div className="w-full px-4 mt-5 overflow-auto hover:overflow-scroll border rounded shadow-md">
+          <table className="table-fixed text-center mb-2">
+            <thead>
+              <tr>
+                <th className="w-1/4 px-1 py-2 text-gray-600">Full Name</th>
+                <th className="w-1/4 px-1 py-2 text-gray-600">Contact Number</th>
+                <th className="w-1/4 px-1 py-2 text-gray-600">Email</th>
+                <th className="w-1/1 px-1 py-2 text-gray-600">Action</th>
               </tr>
             </thead>
             <tbody>
-              {admins.length <= 0 ? (
-                <tr className='border px-4 py-2 bg-red-50'>
+              {employees.length <= 0 ? (
+                <tr className="border px-4 py-2 bg-red-50">
                   <td></td>
                   <td></td>
-                  <td className='px-4 py-2 bg-red-50'>No Employee Data</td>
+                  <td className="px-4 py-2 bg-red-50">No Employee Data</td>
                   <td></td>
                   <td></td>
                 </tr>
               ) : (
-                admins.map((admin, index) => {
-                  return (
-                    <tr key={index}>
-                      <td className='border px-4 py-2 bg-gray-50'>
-                        {admin.id}
-                      </td>
-                      <td className='border px-4 py-2 '>
-                        {admin.firstname + ' ' + admin.lastname}{' '}
-                      </td>
-                      <td className='border px-4 py-2 bg-gray-50'>
-                        {admin.contactnumber}
-                      </td>
-                      <td className='border px-4 py-2'>{admin.address}</td>
-                      <td className='border px-4 py-2 bg-gray-50'>
-                        {admin.email}
-                      </td>
-                      <td className='border px-4 py-2'>
-                        <button
-                          className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mb-2 rounded focus:outline-none focus:shadow-outline w-full text-sm'
-                          onClick={() => deleteAdmin(admin.id)}
-                        >
-                          <DeleteForever className='text-lg' />
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })
+                employees.map((employee, index) => (
+                  <tr key={index}>
+                    <td className="border px-4 py-2">{employee.firstname + ' ' + employee.lastname}</td>
+                    <td className="border px-4 py-2 bg-gray-50">{employee.contactNumber}</td>
+                    <td className="border px-4 py-2 bg-gray-50">{employee.email}</td>
+                    <td className="border px-4 py-2">
+                      {/* <button
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mb-2 rounded focus:outline-none focus:shadow-outline w-full text-sm"
+                        onClick={() => deleteEmployee(employee._id)}
+                      >
+                        <DeleteForever className="text-lg" />
+                      </button> */}
+                      <Button>View</Button>
+                    </td>
+                  </tr>
+                ))
               )}
             </tbody>
           </table>
