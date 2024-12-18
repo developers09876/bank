608b63f88177376914618d0c46eeda571a0cafd5
Anoop_Ajith_branch <your.email@example.com>
2024-12-18 17:16:04 +0530

    testimonial ui completed

--- Frontend/src/components/dashboard/admin/LoanManagement.js
+++ Frontend/src/components/dashboard/admin/LoanManagement.js
@@ -23,7 +23,7 @@ const LoanManagement = ({ collapsed }) => {
         headers: { Authorization: localStorage.getItem("token") },
       });
       const loans = await response.json();
-      setLoan(loans); // Ensure backend provides status for each loan
+      setLoan(loans);
     } catch (error) {
       console.log(error);
     }
@@ -322,7 +322,6 @@ const LoanManagement = ({ collapsed }) => {
                   </Button>
                 )}
 
-                {/* Display "Already Approved" or "Already Rejected" */}
                 {selectedRecord && selectedRecord.status === "1" && (
                   <Button type="primary" disabled>
                     Approved
