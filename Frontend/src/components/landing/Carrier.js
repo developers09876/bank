6faee12f3d50185dda6196c90f99be415e1904cb
SriKumar982 <ksri33138@gmail.com>
2024-12-20 14:04:49 +0530

    carrier changes

--- Frontend/src/components/landing/Carrier.js
+++ Frontend/src/components/landing/Carrier.js
@@ -1,39 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap'
 import Header from '../Layout/Header';
 import Footer from '../Layout/Footer';
 import { Card, Col, Row } from 'antd';
 import './Carrier.css';
 import img1 from '../Images/group-1000002437.png'
+import { FaRegClock } from "react-icons/fa6";
+import { MdBusinessCenter } from "react-icons/md";
+import { BsCashCoin } from "react-icons/bs";
+import { GoClockFill } from "react-icons/go";
 import { ImLocation } from "react-icons/im";
+import axios from 'axios';
 
 function Carrier() {
   const [showForm, setShowForm] = useState(false);
   const [selectedJob, setSelectedJob] = useState(null);
+  const [jobListings, setJobListings] = useState([]);
+  console.log('jobListings', jobListings);
+
+  const [loading, setLoading] = useState(true);
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     resume: null,
   });
 
-  const jobListings = [
-    {
-      title: 'Loan Officer',
-      department: 'Sales',
-      location: 'Remote',
-      type: 'Full-Time',
-      salary: '3 LPA',
-      description: 'Help customers through the loan application process and provide guidance on financial products.',
-    },
-    {
-      title: 'Marketing Specialist',
-      department: 'Marketing',
-      location: 'In-Office',
-      type: 'Part-Time',
-      salary: '5 LPA',
-      description: 'Create and manage marketing campaigns to drive customer engagement and growth.',
-    },
-  ];
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/job/getall');
+        setJobListings(response?.data);
+        console.log('response', response.data)
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching job listings:', error);
+        // setError('Failed to fetch job listings. Please try again later.');
+        setLoading(false);
+      }
+    };
+
+    fetchJobs();
+  }, []);
 
   const handleApplyClick = (job) => {
     setSelectedJob(job);
@@ -73,155 +80,128 @@ function Carrier() {
             our open positions and find a career that matches your passion and skills.
           </p>
         </section>
+      </div>
 
-        <section className="job-listings">
-          <h2>Current Job Openings</h2>
+      <div className='career-container'>
+        <h3 style={{ fontWeight: 'bold', padding: '20px', textAlign: 'center' }}>Current Job Openings</h3>
+        <Row>
           {jobListings.map((job, index) => (
-            <div className="job" key={index}>
-              <h3>{job.title}</h3>
-              <p>
-                <strong>Department:</strong> {job.department}
-              </p>
-              <p>
-                <strong>Location:</strong> {job.location}
-              </p>
-              <p>
-                <strong>Type:</strong> {job.type}
-              </p>
-              <p>
-                <strong>Salary:</strong> {job.salary}
-              </p>
-              <p>
-                <strong>Job Description:</strong> {job.description}
-              </p>
-              <button className="apply-btn" onClick={() => handleApplyClick(job)}>Apply Now</button>
-            </div>
+            <Col key={index} lg={12} className='px-3'>
+              <Card className=' jobcards'>
+                <h5><strong>{job.jobTitle}</strong></h5>
+                <b>{job.company}</b>
+                <p style={{ paddingTop: '5px' }} ><ImLocation style={{ display: 'inline', color: '#1a2a41' }} /><span >{job.location}</span></p>
+                <Row style={{ display: 'flex', justifyContent: 'space-around' }}>
+                  <div className='px-1 py-1'>
+                    <p className='job-role'>
+                      <MdBusinessCenter style={{ display: 'inline' }} /><span> ROLE</span>
+                    </p>
+                    <p className='job-mini'>{job.designation}</p>
+                  </div>
+                  <div className='px-1 py-1'>
+                    <p className='job-role'>
+                      <GoClockFill style={{ display: 'inline' }} /> JOB TYPE</p>
+                    <p className='job-mini'>{job.jobType}</p>
+                  </div>
+                  <div className='px-1 py-1'>
+                    <p className='job-role'>
+                      <BsCashCoin style={{ display: 'inline' }} /> SALARY</p>
+                    <p className='job-mini'>{job.salary} LPA</p>
+                  </div>
+                </Row>
+                <p>{job.description}</p>
+                <p><strong>Requirements :</strong> {job.requirements}</p>
+                {/* <Row className='py-2' style={{ display: 'flex' }}>
+                  {job.requirements.map((requirement, reqIndex) => (
+                    <div className='px-2' key={reqIndex}>
+                      <p className='job-minireq'>{requirement}</p>
+                    </div>
+                  ))}
+                </Row> */}
+                <Button className='applybtn' onClick={() => handleApplyClick(job)}>Apply</Button>
+              </Card>
+            </Col>
           ))}
-        </section>
+        </Row>
+      </div>
 
-        <div>
-          <Row>
-            {jobListings.map((job, index) => (
-              <Col key={index}>
-                <Card>
-                  <h5><strong>{job.title}</strong></h5>
-                  <b>{job.department}</b>
-                  <p style={{ display: 'inline' }}><ImLocation style={{ display: 'inline' }} /><span>{job.location}</span></p>
-                  <Row className='px-2 py-3'>
-                    <Col><p></p></Col>
-                    <Col><p></p></Col>
-                  </Row>
-                </Card>
-              </Col>
-            ))}
-          </Row>
-        </div>
-
-        {/* {showForm && selectedJob && (
-          <div className="modal">
-            <div className="modal-content">
-              <button className="close-btn" onClick={handleCloseForm}>
-                &times;
-              </button>
-              <section className="application-form">
-                <h2>Apply for {selectedJob.title}</h2>
-                <form
-                  action="/submit-application"
-                  method="POST"
-                  encType="multipart/form-data"
-                >
-                  <label htmlFor="name">Full Name:</label>
-                  <input type="text" id="name" name="name" required />
-
-                  <label htmlFor="email">Email Address:</label>
-                  <input type="email" id="email" name="email" required />
-
-                  <label htmlFor="position">Position Applying For:</label>
-                  <select id="position" name="position" defaultValue={jobListings.title}>
-                    <option value={selectedJob.title}>{selectedJob.title}</option>
-                  </select>
-                  <label htmlFor="resume">Upload Resume:</label>
-                  <input
-                    type="file"
-                    id="resume"
-                    name="resume"
-                    accept=".pdf, .doc"
+      <Modal show={showForm} onHide={() => setShowForm(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Apply for {selectedJob ? selectedJob.jobTitle : ''}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={handleFormSubmit}>
+            <Row>
+              <Col className='px-2 py-1' lg={24}>
+                <Form.Group controlId="name">
+                  <Form.Label>Full Name:</Form.Label>
+                  <Form.Control
+                    type="text"
                     required
+                    value={formData.name}
+                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
                   />
+                </Form.Group>
+              </Col>
 
-                  <button type="submit" className="submit-button1">
-                    Submit Application
-                  </button>
-                </form>
-              </section>
-            </div>
-          </div>
-        )} */}
-        <Modal show={showForm} onHide={() => setShowForm(false)}>
-          <Modal.Header closeButton>
-            <Modal.Title>Apply for {selectedJob ? selectedJob.title : ''}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={handleFormSubmit}>
-              <Row>
-                <Col className='px-2 py-2' lg={12}>
-                  <Form.Group controlId="name">
-                    <Form.Label>Full Name:</Form.Label>
-                    <Form.Control
-                      type="text"
-                      required
-                      value={formData.name}
-                      onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                    />
-                  </Form.Group>
-                </Col>
+              <Col className='px-2 py-1' lg={24}>
+                <Form.Group controlId="number">
+                  <Form.Label>Phone Number:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    required
+                    value={formData.number}
+                    onChange={(e) => setFormData({ ...formData, number: e.target.value })}
+                  />
+                </Form.Group>
+              </Col>
 
-                <Col className='px-2 py-2' lg={12}>
-                  <Form.Group controlId="email">
-                    <Form.Label>Email Address:</Form.Label>
-                    <Form.Control
-                      type="email"
-                      required
-                      value={formData.email}
-                      onChange={(e) => setFormData({ ...formData, email: e.target.value })}
-                    />
-                  </Form.Group>
-                </Col>
+              <Col className='px-2 py-1' lg={24}>
+                <Form.Group controlId="email">
+                  <Form.Label>Email Address:</Form.Label>
+                  <Form.Control
+                    type="email"
+                    required
+                    value={formData.email}
+                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                  />
+                </Form.Group>
+              </Col>
 
-                <Col className='px-2 py-2' lg={12}>
-                  <Form.Group controlId="position">
-                    <Form.Label>Position Applying For:</Form.Label>
-                    <Form.Control
-                      as="select"
-                      disabled
-                      value={selectedJob ? selectedJob.title : ''}
-                    >
-                      <option>{selectedJob ? selectedJob.title : ''}</option>
-                    </Form.Control>
-                  </Form.Group>
-                </Col>
+              <Col className='px-2 py-1' lg={24}>
+                <Form.Group controlId="position">
+                  <Form.Label>Position Applying For:</Form.Label>
+                  <Form.Control
+                    as="select"
+                    disabled
+                    value={selectedJob ? selectedJob.jobTitle : ''}
+                  >
+                    <option>{selectedJob ? selectedJob.jobTitle : ''}</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
 
-                <Col className='px-2 py-2' lg={12}>
-                  <Form.Group controlId="resume">
-                    <Form.Label>Upload Resume:</Form.Label>
-                    <Form.Control
-                      type="file"
-                      // style={{width:'fit-content'}}
-                      required
-                      onChange={(e) => setFormData({ ...formData, resume: e.target.files[0] })}
-                    />
-                  </Form.Group>
-                </Col>
-              </Row>
-              <br />
-              <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Button variant="primary" type="submit">
-                  Submit
-                </Button></div>
-            </Form>
-          </Modal.Body>
-        </Modal>
-
+              <Col className='px-2 py-1' lg={24}>
+                <Form.Group controlId="resume">
+                  <Form.Label>Upload Resume:</Form.Label>
+                  <Form.Control
+                    type="file"
+                    // style={{width:'fit-content'}}
+                    required
+                    onChange={(e) => setFormData({ ...formData, resume: e.target.files[0] })}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <br />
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button></div>
+          </Form>
+        </Modal.Body>
+      </Modal>
+      <div className='career-container'>
         <section className="employee-testimonials">
           <h2>Why Our Employees Love Working Here</h2>
           <blockquote>
