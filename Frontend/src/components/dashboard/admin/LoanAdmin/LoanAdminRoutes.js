dd5e16c3be5680b33f33fb85d61a1d088247605b
Karthi-code01 <karthiuced@gmail.com>
2025-01-09 15:55:40 +0530

    done

--- Frontend/src/components/dashboard/admin/LoanAdmin/LoanAdminRoutes.js
+++ Frontend/src/components/dashboard/admin/LoanAdmin/LoanAdminRoutes.js
@@ -4,11 +4,13 @@ import LoanDashboard from "./LoanDashboard";
 import LoanLeadManagement from "./LoanLeadManagement";
 import EmployeeUserList from "./EmployeeUserList";
 import TabsProfile from "./ProfileTabs";
+import LoanLeadDetails from "./LoanLeadDetails";
 
 export const LoanAdminRoutes = [
   { path: "", element: <LoanDashboard /> },
   { path: "loanmanagement", element: <LoanManagement /> },
   { path: "leadmanagement", element: <LoanLeadManagement /> },
+  { path: "leaddetails", element: <LoanLeadDetails /> },
   { path: "/adminLoan/createlead", element: <CreateLead /> },
   { path: "employeeuserlist", element: <EmployeeUserList /> },
   { path: "myprofile", element: <TabsProfile /> },
