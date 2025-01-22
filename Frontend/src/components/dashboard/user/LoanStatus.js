4fa0616a0ad73638183fd1b2f772875c992b40f3
SriKumar982 <ksri33138@gmail.com>
2025-01-22 14:21:02 +0530

    changes in view edit of loan

--- Frontend/src/components/dashboard/user/LoanStatus.js
+++ Frontend/src/components/dashboard/user/LoanStatus.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Table, Tag, Button } from "antd";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { EyeOutlined, EditOutlined } from "@ant-design/icons";
 
 const LoanStatusTable = ({ collapsed }) => {
   const [loans, setLoans] = useState([]);
@@ -70,13 +71,32 @@ const LoanStatusTable = ({ collapsed }) => {
       key: "action",
       render: (text, record) => {
         return (
-          <Button
-            type="primary"
-            style={{ background: "#4096ff", color: "#fff" }}
-            onClick={() => handleViewDetails(record)}
-          >
-            View
-          </Button>
+          // <Button
+          //   type="primary"
+          //   style={{ background: "#4096ff", color: "#fff" }}
+          //   onClick={() => handleViewDetails(record)}
+          // >
+          //   View
+          // </Button>
+          <>
+        <EyeOutlined
+          style={{
+            fontSize: "18px",
+            color: "#4096ff",
+            cursor: "pointer",
+            marginRight: "15px",
+          }}
+          onClick={() => handleViewDetails(record)}
+        />
+        <EditOutlined
+          style={{
+            fontSize: "18px",
+            color: "#ff4d4f",
+            cursor: "pointer",
+          }}
+          // onClick={() => handleEdit(record)}
+        />
+      </>
         );
       },
     },
