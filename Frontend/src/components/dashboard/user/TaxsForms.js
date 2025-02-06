5a67c7aa581a91dc7c50f636506770326eed9cf7
SriKumar982 <ksri33138@gmail.com>
2025-02-06 11:12:13 +0530

    tax subCategory changes in home page

--- Frontend/src/components/dashboard/user/TaxsForms.js
+++ Frontend/src/components/dashboard/user/TaxsForms.js
@@ -17,7 +17,7 @@ function TaxsForms() {
     formState: { errors },
   } = useForm();
 
-  const category = watch("employeeCategory");
+  const category = watch("taxType");
 
   const id = localStorage.getItem("id");
   const userType = localStorage.getItem("userType");
@@ -34,10 +34,10 @@ function TaxsForms() {
       panno: data.panno,
       gst: data.gst,
       taxType: data.taxType,
+      subCategory: data.subCategory,
       incomeTaxStatus: data.incomeTaxStatus,
       businessType: data.businessType,
       annualIncome: data.annualIncome,
-      taxPaid: data.taxPaid,
     };
     console.log("details", details);
     try {
@@ -206,7 +206,7 @@ function TaxsForms() {
               <div>
                 <label className="vendorpage_labelCss">Category:</label>
                 <Controller
-                  name="taxtype"
+                  name="taxType"
                   control={control}
                   defaultValue=""
                   rules={{ required: true }}
@@ -214,10 +214,10 @@ function TaxsForms() {
                     <Select
                       {...field}
                       className="inputcolumn_drp"
-                      placeholder="Select taxtype"
+                      placeholder="Select taxType"
                       onChange={(value) => {
                         field.onChange(value);
-                        setValue("taxtype", value);
+                        setValue("taxType", value);
                       }}
                     >
                       <Option value="">Select Category</Option>
@@ -230,7 +230,7 @@ function TaxsForms() {
                     </Select>
                   )}
                 />
-                {errors.employeeCategory && (
+                {errors.taxType && (
                   <p className="text-danger">Employee category is required</p>
                 )}
               </div>
@@ -255,10 +255,17 @@ function TaxsForms() {
                           setValue("subCategory", value);
                         }}
                       >
-                        <Option value="Company">Company</Option>
-                        <Option value="Individual">Individual</Option>
-                        <Option value="Firm">Firm</Option>
-                        <Option value="Other">Other</Option>
+                        <Option value="">Select Sub-category</Option>
+                        <Option value="Annual return Filling(Individual)">
+                          Annual return Filling(Individual)
+                        </Option>
+                        <Option value="Annual return Filling(Firm/Company)">
+                          Annual return Filling(Firm/Company)
+                        </Option>
+                        <Option value="Company Registration">
+                          Company Registration
+                        </Option>
+                        <Option value="Notice Services">Notice Services</Option>
                       </Select>
                     )}
                   />
@@ -266,6 +273,44 @@ function TaxsForms() {
                     <p className="text-danger">Sub-category is required</p>
                   )}
                 </div>
+              </Col>
+            )}
+            {/* Subcategory for GST Services */}
+            {(category === "GSTservices" ||
+              category === "Tds&TcsServices" ||
+              category === "Esi&PfServices") && (
+              <Col xs={12} md={6} lg={4}>
+                <label className="vendorpage_labelCss">Sub Category:</label>
+                <Controller
+                  name="subCategory"
+                  control={control}
+                  defaultValue=""
+                  rules={{ required: true }}
+                  render={({ field }) => (
+                    <Select
+                      {...field}
+                      className="inputcolumn_drp"
+                      placeholder="Select Sub Category"
+                      onChange={(value) => setValue("subCategory", value)}
+                    >
+                      <Option value="">Select Sub-category</Option>
+                      <Option value="Registration">Registration</Option>
+                      <Option value="Monthly Return Filing">
+                        Monthly Return Filing
+                      </Option>
+                      <Option value="Quaterly Return Filing">
+                        Quaterly Return Filing
+                      </Option>
+                      <Option value="Annual Return Filing">
+                        Annual Return Filing
+                      </Option>
+                      <Option value="Notice Services">Notice Services</Option>
+                    </Select>
+                  )}
+                />
+                {errors.subCategory && (
+                  <p className="text-danger">Sub-category is required</p>
+                )}
               </Col>
             )}
 
@@ -337,24 +382,6 @@ function TaxsForms() {
                 />
                 {errors.annualIncome && (
                   <p className="text-danger">Enter annual income</p>
-                )}
-              </div>
-            </Col>
-
-            <Col xs={12} md={6} lg={4}>
-              <div>
-                <label className="vendorpage_labelCss">
-                  Tax Paid in Last Year
-                </label>
-                <input
-                  className="inputcolumn-ourProfile"
-                  type="number"
-                  name="taxPaid"
-                  {...register("taxPaid", { required: true })}
-                  placeholder="Tax Paid"
-                />
-                {errors.taxPaid && (
-                  <p className="text-danger">Enter tax paid last year</p>
                 )}
               </div>
             </Col>
