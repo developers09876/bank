6ce8d3c6677beddf50ca4360a5e26ab301da22b2
Karthi-code01 <karthiuced@gmail.com>
2025-02-07 21:53:33 +0530

    done

--- Frontend/src/components/dashboard/Employee/EmployeeRoutes.js
+++ Frontend/src/components/dashboard/Employee/EmployeeRoutes.js
@@ -31,6 +31,7 @@ import TaxTaskViewDetails from "./TaxTaskViewDetails";
 import InsuranceManagementEdit from "./InsuranceManagementEdit";
 import TaxsManagementEdit from "./TaxManagementEdit";
 import LoanProfileTab from "./LoanManagementForm/LoanProfileTabs";
+import UserLoanFormProfileTab from "../../landing/LoanForm/LoanFormProfileTab";
 
 export const EmployeeRoutes = [
   {
@@ -144,5 +145,9 @@ export const EmployeeRoutes = [
   {
     path: "/employee/editownloan/:id",
     element: <LoanProfileTab />,
+  },
+  {
+    path: "/employee/createloan",
+    element: <UserLoanFormProfileTab />,
   },
 ];
