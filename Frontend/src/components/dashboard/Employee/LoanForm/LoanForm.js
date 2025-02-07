3a83c0c8c7a32356709f40ae6cd8c6f63b19b9cd
Karthi-code01 <karthiuced@gmail.com>
2025-02-07 15:01:09 +0530

    done

--- Frontend/src/components/dashboard/Employee/LoanForm/LoanForm.js
+++ Frontend/src/components/dashboard/Employee/LoanForm/LoanForm.js
@@ -140,7 +140,7 @@ const { state } = useLocation();
   useEffect(() => {
     const fetchLoanApplicationData = async () => {
       try {
-        const response = await Api.get(`/loanform/getbyid/${userid}`);
+        const response = await Api.get(`/loanform/getbyEmployeeid/${userid}`);
         console.log('response', response)
         const filterOneApplication = response.data.filter((application) => application._id === record._id )
         console.log('Applicationresponse', response.data);
