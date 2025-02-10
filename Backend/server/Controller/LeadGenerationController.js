e414010d5a472bdf9680700843a866234f07ac75
Subitha879 <subithasubi489@gmail.com>
2025-02-10 18:06:45 +0530

    LeadDetails changes push

--- Backend/server/Controller/LeadGenerationController.js
+++ Backend/server/Controller/LeadGenerationController.js
@@ -149,18 +149,42 @@ export async function getById(req, res, next) {
     });
   }
 }
-export const getByLeadEmployeeID= async (req, res) => {
+
+export async function  getByLeadDetailsID(req, res, next) {
   try {
-    const { employeeId } = req.params;
-    const lead = await Lead.find({ employeeId });
-    if (!lead) {
-      return res.status(404).json({ message: "User id not found" });
+    const { id } = req.params;
+    const leads = await Lead.find({ _id: id });
+
+    if (leads && leads.length > 0) {
+      res.status(200).json({
+        message: "Leads fetched successfully",
+        data: leads,
+      });
+    } else {
+      res.status(404).json({
+        message: "No leads found for the given user ID",
+      });
     }
-    res.status(200).json(lead);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.error("Error fetching leads by ID:", error);
+    res.status(500).json({
+      message: "An error occurred while fetching leads",
+      error: error.message,
+    });
   }
-};
+}
+// export const getByLeadEmployeeID= async (req, res) => {
+//   try {
+//     const { employeeId } = req.params;
+//     const lead = await Lead.find({ employeeId });
+//     if (!lead) {
+//       return res.status(404).json({ message: "User id not found" });
+//     }
+//     res.status(200).json(lead);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
 
 export async function getByLeadId(req, res, next) {
   try {
