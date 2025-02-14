0de00e08580e565b1e736e6573eaa63efad64130
Subitha879 <subithasubi489@gmail.com>
2025-02-14 17:21:35 +0530

    rewads subscription media query

--- Frontend/src/components/Layout/Footer.js
+++ Frontend/src/components/Layout/Footer.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { FaFacebookF, FaInstagram, FaWhatsapp, FaTwitter } from "react-icons/fa";
+import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaWhatsapp,
+  FaTwitter,
+} from "react-icons/fa";
 
 function Footer() {
   return (
     <div>
       <footer className="bg-gray-100 text-gray-700">
         <div className="max-w-screen-xl p-6 py-8 mx-auto lg:py-12 md:px-6 lg:px-8">
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
-            
-            <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 text-center md:text-left">
+          <div>
               <h3 className="mb-4 text-lg font-bold text-gray-900">VILU GENIUS</h3>
               <p className="mb-2 text-sm text-gray-500">
                 The genesis of VILU Genius Private Limited is traced to Mr. Prakasha H for his decade of experience in the retail banking industry, primarily in housing loans and financial services.
@@ -45,21 +49,25 @@ function Footer() {
             </div>
 
             <div>
-              <h3 className="mb-4 text-lg font-bold text-gray-900">Contact Us</h3>
-              <p className="text-sm text-gray-500 mb-2">
+              <h3 className="mb-4 text-lg font-bold text-gray-900">
+                Contact Us
+              </h3>
+              <p className="text-sm text-gray-500">
                 <strong>Email:</strong> vilugenius@gmail.com
               </p>
-              <p className="text-sm text-gray-500 mb-2">
-                <strong>Phone:</strong> +91 9620795762<br />+91 9900541268
+              <p className="text-sm text-gray-500">
+                <strong>Phone:</strong> +91 9620795762
               </p>
               <p className="text-sm text-gray-500">
-                <strong>Address:</strong> Registered address of Vilu Genius is 26832/20/3A, Someshwara Extension, 1st Main, Doddaballapur, Karnataka, India-561203.
+                <strong>Address:</strong>Registered address of Vilu Genius is 26832/20/3A, <br/> Someshwara Extension,<br/>  1st Main, Doddaballapur,<br/>  Karnataka, India-561203.
               </p>
             </div>
 
-            <div className="text-center">
-              <h3 className="mb-4 text-lg font-bold text-gray-900">Follow Us</h3>
-              <ul className="flex justify-center space-x-6" style={{paddingLeft:"4rem"}}>
+            <div className="text-center md:text-left">
+              <h3 className="mb-4 text-lg font-bold text-gray-900">
+                Follow Us
+              </h3>
+              <ul className="flex justify-center md:justify-start space-x-6">
                 <li>
                   <a href="#" className="text-gray-500 hover:text-blue-500">
                     <FaFacebookF className="w-7 h-7" />
@@ -84,9 +92,9 @@ function Footer() {
             </div>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto" />
-          <div style={{ textAlign: "center", marginTop: "25px" }}>
+          <div className="text-center mt-6">
             <span className="text-sm text-gray-500">
-              © 2022-2023 VILU GENIUS™. All Rights Reserved. | <a href="#" className="hover:text-blue-500">Terms & Conditions</a> | <a href="#" className="hover:text-blue-500">Privacy Policy</a>
+              © 2022-2023 VILU GENIUS™. All Rights Reserved.
             </span>
           </div>
         </div>
