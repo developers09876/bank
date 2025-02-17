34e4d45216a1be0758174d679ce1b0725258ccaf
Anoop_Ajith_branch <your.email@example.com>
2025-02-17 13:26:43 +0530

    changes

--- Frontend/src/components/landing/Carrier.js
+++ Frontend/src/components/landing/Carrier.js
@@ -1,27 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap'
-import Header from '../Layout/Header';
-import Footer from '../Layout/Footer';
-import { Card, Col, Row } from 'antd';
-import './Carrier.css';
-import img1 from '../Images/group-1000002437.png'
+import React, { useEffect, useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import Header from "../Layout/Header";
+import Footer from "../Layout/Footer";
+import { Card, Col, Row } from "antd";
+import "./Carrier.css";
+import img1 from "../Images/group-1000002437.png";
 import { FaRegClock } from "react-icons/fa6";
 import { MdBusinessCenter } from "react-icons/md";
 import { BsCashCoin } from "react-icons/bs";
 import { GoClockFill } from "react-icons/go";
 import { ImLocation } from "react-icons/im";
-import axios from 'axios';
+import axios from "axios";
 import { toast, ToastContainer } from "react-toastify";
 
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 
 function Carrier() {
-
   const [showForm, setShowForm] = useState(false);
   const [selectedJob, setSelectedJob] = useState(null);
   const [jobListings, setJobListings] = useState([]);
   const [selectImage, setSelectImage] = useState(null);
-  console.log('selectImage', selectImage)
+  console.log("selectImage", selectImage);
   const userId = localStorage.getItem("id");
   const {
     register,
@@ -33,10 +32,10 @@ function Carrier() {
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/job/getall');
+        const response = await axios.get("http://localhost:5000/job/getall");
         setJobListings(response?.data);
       } catch (error) {
-        console.error('Error fetching job listings:', error);
+        console.error("Error fetching job listings:", error);
       }
     };
 
@@ -85,7 +84,7 @@ function Carrier() {
       phone: data.phone,
       resume: data.resume,
     };
-    console.log('rsssss', details.jobTitle)
+    console.log("rsssss", details.jobTitle);
     try {
       if (selectImage) {
         const uploadedResumeUrl = await uploadFile(selectImage);
@@ -99,7 +98,7 @@ function Carrier() {
       data.jobTitle = selectedJob?.jobTitle;
 
       const response = await axios.post(
-        'http://localhost:5000/jobrequest/createjobrequest',
+        "http://localhost:5000/jobrequest/createjobrequest",
         details
       );
 
@@ -107,12 +106,11 @@ function Carrier() {
         toast.success("Job Applied successfully");
         setShowForm(false);
         reset();
-
       } else {
-        alert('Failed to submit job application. Please try again later.');
+        alert("Failed to submit job application. Please try again later.");
       }
     } catch (error) {
-      console.error('Error submitting job application:', error);
+      console.error("Error submitting job application:", error);
       toast.error("An error occurred while submitting the form");
     }
   };
@@ -134,68 +132,68 @@ function Carrier() {
           <img src={img1} alt="Description of Image" style={{ width: '100%', height: 'auto' }} />
         </Col>
       </Row> */}
-           <Row
-      style={{
-        height: '90vh',
-        padding: '30px',
-        marginTop: '74px',
-        display: 'flex',
-        flexWrap: 'wrap',
-      }}
-    >
-      {/* Left Column */}
-      <Col
-        lg={12}
-        md={12}
-        sm={24}
-        xs={24}
-        className="career-text"
+      <Row
         style={{
-          backgroundColor: '#4169E1',
-          padding: '150px 200px 0px 50px',
-          color: 'white',
-          textAlign: 'left',
+          height: "90vh",
+          padding: "30px",
+          marginTop: "74px",
+          display: "flex",
+          flexWrap: "wrap",
         }}
       >
-        <h1 className="career-heading">
-          Kickstart Your <br /> Career with{' '}
-          <span style={{ color: '#800000' }}>Us</span>
-        </h1>
-        <h4 className="career-subtext">
-          Join our dynamic team and help people achieve their financial goals
-          through our innovative loan solutions.
-        </h4>
-      </Col>
-
-      {/* Right Column (Image) */}
-      <Col
-        lg={12}
-        md={12}
-        sm={24}
-        xs={24}
-        className="career-image"
-        style={{
-          backgroundColor: '#4169E1',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <img
-          src={img1}
-          alt="Career"
-          className="career-img"
+        {/* Left Column */}
+        <Col
+          lg={12}
+          md={12}
+          sm={24}
+          xs={24}
+          className="career-text"
           style={{
-            width: '100%',
-            height: 'auto',
-            maxWidth: '600px', 
+            backgroundColor: "#4169E1",
+            padding: "150px 200px 0px 50px",
+            color: "white",
+            textAlign: "left",
           }}
-        />
-      </Col>
+        >
+          <h1 className="career-heading">
+            Kickstart Your <br /> Career with{" "}
+            <span style={{ color: "#800000" }}>Us</span>
+          </h1>
+          <h4 className="career-subtext">
+            Join our dynamic team and help people achieve their financial goals
+            through our innovative loan solutions.
+          </h4>
+        </Col>
 
-      {/* Media Queries for Mobile (320px), Tablet (768px), and Laptop (1024px) */}
-      <style>
-        {`
+        {/* Right Column (Image) */}
+        <Col
+          lg={12}
+          md={12}
+          sm={24}
+          xs={24}
+          className="career-image"
+          style={{
+            backgroundColor: "#4169E1",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={img1}
+            alt="Career"
+            className="career-img"
+            style={{
+              width: "100%",
+              height: "auto",
+              maxWidth: "600px",
+            }}
+          />
+        </Col>
+
+        {/* Media Queries for Mobile (320px), Tablet (768px), and Laptop (1024px) */}
+        <style>
+          {`
           @media (max-width: 1024px) { /* Laptop View */
             .career-heading {
               font-size: 40px !important;
@@ -241,49 +239,68 @@ function Carrier() {
             }
           }
         `}
-      </style>
-    </Row>
+        </style>
+      </Row>
       <br />
 
       <div className="career-container">
         <section className="career-intro">
           <h1>Join Our Team</h1>
           <p>
-            We are a fast-growing loan company committed to empowering people to achieve their financial goals. Explore
-            our open positions and find a career that matches your passion and skills.
+            We are a fast-growing loan company committed to empowering people to
+            achieve their financial goals. Explore our open positions and find a
+            career that matches your passion and skills.
           </p>
         </section>
       </div>
 
-      <div className='career-container'>
-        <h3 style={{ fontWeight: 'bold', padding: '20px', textAlign: 'center' }}>Current Job Openings</h3>
+      <div className="career-container">
+        <h3
+          style={{ fontWeight: "bold", padding: "20px", textAlign: "center" }}
+        >
+          Current Job Openings
+        </h3>
         <Row>
           {jobListings.map((job, index) => (
-            <Col key={index} lg={12} className='px-3'>
-              <Card className=' jobcards'>
-                <h5><strong>{job.jobTitle}</strong></h5>
+            <Col key={index} lg={12} className="px-3" style={{ width: "100%" }}>
+              <Card className=" jobcards">
+                <h5>
+                  <strong>{job.jobTitle}</strong>
+                </h5>
                 <b>{job.company}</b>
-                <p style={{ paddingTop: '5px' }} ><ImLocation style={{ display: 'inline', color: '#1a2a41' }} /><span >{job.location}</span></p>
-                <Row style={{ display: 'flex', justifyContent: 'space-around' }}>
-                  <div className='px-1 py-1'>
-                    <p className='job-role'>
-                      <MdBusinessCenter style={{ display: 'inline' }} /><span> ROLE</span>
+                <p style={{ paddingTop: "5px" }}>
+                  <ImLocation style={{ display: "inline", color: "#1a2a41" }} />
+                  <span>{job.location}</span>
+                </p>
+                <Row
+                  style={{ display: "flex", justifyContent: "space-around" }}
+                >
+                  <div className="px-1 py-1">
+                    <p className="job-role">
+                      <MdBusinessCenter style={{ display: "inline" }} />
+                      <span> ROLE</span>
                     </p>
-                    <p className='job-mini'>{job.designation}</p>
+                    <p className="job-mini">{job.designation}</p>
                   </div>
-                  <div className='px-1 py-1'>
-                    <p className='job-role'>
-                      <GoClockFill style={{ display: 'inline' }} /> JOB TYPE</p>
-                    <p className='job-mini'>{job.jobType}</p>
+                  <div className="px-1 py-1">
+                    <p className="job-role">
+                      <GoClockFill style={{ display: "inline" }} /> JOB TYPE
+                    </p>
+                    <p className="job-mini">{job.jobType}</p>
                   </div>
-                  <div className='px-1 py-1'>
-                    <p className='job-role'>
-                      <BsCashCoin style={{ display: 'inline' }} /> SALARY</p>
-                    <p className='job-mini'>{job.salary} LPA</p>
+                  <div className="px-1 py-1">
+                    <p className="job-role">
+                      <BsCashCoin style={{ display: "inline" }} /> SALARY
+                    </p>
+                    <p className="job-mini">{job.salary} LPA</p>
                   </div>
                 </Row>
-                <p>{job.description}</p><br />
-                <p ><strong>Requirements :</strong> {job.requirements}</p><br />
+                <p>{job.description}</p>
+                <br />
+                <p>
+                  <strong>Requirements :</strong> {job.requirements}
+                </p>
+                <br />
                 {/* <Row className='py-2' style={{ display: 'flex' }}>
                   {job.requirements.map((requirement, reqIndex) => (
                     <div className='px-2' key={reqIndex}>
@@ -291,61 +308,73 @@ function Carrier() {
                     </div>
                   ))}
                 </Row> */}
-                <Button className='applybtn' onClick={() => handleApplyClick(job)}>Apply</Button>
+                <Button
+                  className="applybtn"
+                  onClick={() => handleApplyClick(job)}
+                >
+                  Apply
+                </Button>
               </Card>
             </Col>
           ))}
         </Row>
       </div>
 
-
       <Modal show={showForm} onHide={() => setShowForm(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Apply for {selectedJob ? selectedJob.jobTitle : ''}</Modal.Title>
+          <Modal.Title>
+            Apply for {selectedJob ? selectedJob.jobTitle : ""}
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <form onSubmit={handleSubmit(onSubmit)}>
             <Row>
-              <Col className='px-2 py-1' lg={24}>
+              <Col className="px-2 py-1" lg={24}>
                 <label>Full Name:</label>
                 <input
                   type="text"
-                  {...register('name', { required: 'Full Name is required' })}
-                  className='form-control'
+                  {...register("name", { required: "Full Name is required" })}
+                  className="form-control"
                 />
-                {errors.name && <p className='error-message'>{errors.name.message}</p>}
+                {errors.name && (
+                  <p className="error-message">{errors.name.message}</p>
+                )}
               </Col>
 
-              <Col className='px-2 py-1' lg={24}>
+              <Col className="px-2 py-1" lg={24}>
                 <label>Phone Number:</label>
                 <input
                   type="number"
-                  {...register('phone', {
-                    required: 'Phone Number is required',
+                  {...register("phone", {
+                    required: "Phone Number is required",
                     pattern: {
                       value: /^[0-9]{10}$/,
-                      message: 'Enter a valid 10-digit phone number',
+                      message: "Enter a valid 10-digit phone number",
                     },
                   })}
-                  className='form-control'
+                  className="form-control"
                 />
-                {errors.phone && <p className='error-message'>{errors.phone.message}</p>}
+                {errors.phone && (
+                  <p className="error-message">{errors.phone.message}</p>
+                )}
               </Col>
 
-              <Col className='px-2 py-1' lg={24}>
+              <Col className="px-2 py-1" lg={24}>
                 <label>Email Address:</label>
                 <input
                   type="email"
-                  {...register('email', {
-                    required: 'Email is required',
+                  {...register("email", {
+                    required: "Email is required",
                     pattern: {
                       value: /^[^@\s]+@[^@\s]+\.[^@\s]+$/,
-                      message: 'Enter a valid email address',
+                      message: "Enter a valid email address",
                     },
                   })}
-                  className='form-control'
+                  className="form-control"
                 />
-                {errors.email && <p className='error-message'>{errors.email.message}</p>}
+                {errors.email && (
+                  <p className="error-message">{errors.email.message}</p>
+                )}
               </Col>
 
               {/* <Col className='px-2 py-1' lg={24}>
@@ -357,51 +386,58 @@ function Carrier() {
                   className='form-control'
                 />
               </Col> */}
-              <Col className='px-2 py-1' lg={24}>
+              <Col className="px-2 py-1" lg={24}>
                 <label>Position Applying For:</label>
                 <input
                   type="text"
-                  value={selectedJob ? selectedJob.jobTitle : ''}
+                  value={selectedJob ? selectedJob.jobTitle : ""}
                   disabled
                   className="form-control"
                 />
                 <input
                   type="hidden"
-                  {...register('jobTitle', { required: 'Job title is required' })}
-                  value={selectedJob ? selectedJob.jobTitle : ''}
+                  {...register("jobTitle", {
+                    required: "Job title is required",
+                  })}
+                  value={selectedJob ? selectedJob.jobTitle : ""}
                 />
               </Col>
 
-              <Col className='px-2 py-1' lg={24}>
+              <Col className="px-2 py-1" lg={24}>
                 <label>Upload Resume:</label>
                 <input
                   type="file"
-                  {...register('resume', { required: 'Resume is required' })}
+                  {...register("resume", { required: "Resume is required" })}
                   onChange={handleFileChange}
-                  className='form-control'
+                  className="form-control"
                 />
-                {errors.resume && <p className='error-message'>{errors.resume.message}</p>}
+                {errors.resume && (
+                  <p className="error-message">{errors.resume.message}</p>
+                )}
               </Col>
             </Row>
             <br />
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
               <Button variant="primary" type="submit">
                 Submit
-              </Button></div>
+              </Button>
+            </div>
           </form>
         </Modal.Body>
       </Modal>
-      <div className='career-container'>
+      <div className="career-container">
         <section className="employee-testimonials">
           <h2>Why Our Employees Love Working Here</h2>
           <blockquote>
-            "<span style={{ color: 'red' }}>Vilu Genius </span>Loans has been an amazing place to grow my career. The team is supportive, and I love the work we do." – Prakash, Loan Specialist
+            "<span style={{ color: "red" }}>Vilu Genius </span>Loans has been an
+            amazing place to grow my career. The team is supportive, and I love
+            the work we do." – Prakash, Loan Specialist
           </blockquote>
         </section>
       </div>
       <Footer />
     </div>
   );
-};
+}
 
 export default Carrier;
