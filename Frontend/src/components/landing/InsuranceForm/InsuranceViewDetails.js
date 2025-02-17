1f54226b450e9a4b94989013a8e151379190a44f
SriKumar982 <ksri33138@gmail.com>
2025-02-17 19:07:08 +0530

    changes in insurance view [age

--- Frontend/src/components/landing/InsuranceForm/InsuranceViewDetails.js
+++ Frontend/src/components/landing/InsuranceForm/InsuranceViewDetails.js
@@ -79,8 +79,8 @@ const InsuranceViewDetails = ({ collapsed }) => {
               </Tag>
             </div>
           </div>
-          <Row className="px-4 py-3">
-            <Col>
+          <Row className="px-2 py-2">
+            {/* <Col>
               <Card>
                 <Row>
                   <Col
@@ -139,9 +139,33 @@ const InsuranceViewDetails = ({ collapsed }) => {
                   </Col>
                 </Row>
               </Card>
+            </Col> */}
+            <Col lg={12} md={12}>
+              <Card className="loandetail-custom-card" title="Personal Details">
+                <Descriptions column={{ xl: 3, lg: 2, xs: 1, md: 1, sm: 1 }}>
+                  <Descriptions.Item label="Name">
+                    {record.firstname} {record.lastname}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Email Id">
+                    {record.email}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Contact ">
+                    {record.contactNumber}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="Adhaar Number">
+                    {record.aadhar}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="PanCard Number">
+                    {record.panno}
+                  </Descriptions.Item>
+                  <Descriptions.Item label="GST Number">
+                    {record.gst}
+                  </Descriptions.Item>
+                </Descriptions>
+              </Card>
             </Col>
           </Row>
-          <Row style={{ textAlign: "-webkit-center" }}>
+          <Row className="px-2 py-2">
             <Col lg={12} md={12}>
               <Card
                 className="loandetail-custom-card"
@@ -173,7 +197,7 @@ const InsuranceViewDetails = ({ collapsed }) => {
               </Card>
             </Col>
           </Row>
-          <Row>
+          <Row className="px-2 py-2">
             <Col lg={12} md={12}>
               <Card className="loandetail-custom-card" title="Insurance Status">
                 <Descriptions column={{ xl: 3, lg: 2, xs: 1, md: 1, sm: 1 }}>
